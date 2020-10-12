aa23c5ab34acf87c56094a998ae1edbf2186d968
Anthony Herrera <aaherrera3@miners.utep.edu>
2020-10-12 15:12:34 -0600

    Server now with threads, bit buggy

--- file-transfer-lab/Server/fileServer.py
+++ file-transfer-lab/Server/fileServer.py
@@ -1,38 +1,42 @@
-import os, socket, tqdm
+import os, socket, threading, tqdm
+from _thread import *
 
+printLock = threading.Lock() #Protect values againsst simultaneous access from mult threads
 
-address = "" #default ip for server 
-port = 50001 # port number hosting
-bufferSize = 4096 #number of bytes to send each time
+def threaded(c):
+    bufferSize = 4096 #number of bytes to send each time
+    while True: 
+        fileInfo = c.recv(bufferSize).decode() # get file info 
+        fileName, fileSize = fileInfo.split("|") # split file info 
 
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create server socket
-serverSock.bind((address,port)) # bind socket to port 50001 and let it chose the address 
-serverSock.listen(1) # have the server socket in listen mode to 1 connection 
+        fileName = os.path.basename(fileName) # remove absolute path
+        fileSize = int(fileSize) # conver to int
+        progress = tqdm.tqdm(range(fileSize), f"Sending {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
+        
+        with open("(Server)"+fileName, "wb") as f:
+            for _ in progress:
+                bytesRead = c.recv(bufferSize) # recevive 4096 bytes from client
+                if not bytesRead:
+                    print("File Transfer Complete")
+                    break #file transfer done
+                f.write(bytesRead) # Write 4096 to server file
+                progress.update(len(bytesRead)) # update progress bar
+        c.close()
 
-print(f"[+]Waiting for connection.")
-convSock, clienAddress = serverSock.accept() #creation of connection scoket
-print(f"[+] Connection to {clienAddress}.") 
+def Main():
+    address = "" #default ip for server 
+    port = 50000 # port number hosting
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create server socket
+    serverSock.bind((address,port)) # bind socket to port 50001 and let it chose the address 
+    serverSock.listen(5) # have the server socket in listen mode to 5 connection 
+    while True:
+        print(f"[+]Waiting for connection.")
+        convSock, clienAddress = serverSock.accept() #creation of connection scoket
+        printLock.acquire()
+        print(f"[+] Connection to {clienAddress}.")
 
-fileInfo = convSock.recv(bufferSize).decode() # get file info 
-fileName, fileSize = fileInfo.split("|") # split file info 
+        start_new_thread(threaded, (convSock,))
+    serverSock.close()
 
-fileName = os.path.basename(fileName) # remove absolute path
-fileSize = int(fileSize) # conver to int
-
-progress = tqdm.tqdm(range(fileSize), f"Sending {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
-with open("(Server)"+fileName, "wb") as f:
-    for _ in progress:
-        bytesRead = convSock.recv(bufferSize) # recevive 4096 bytes from client
-        if not bytesRead:
-            print("File Transfer Complete")
-            break #file transfer done
-        f.write(bytesRead) # Write 4096 to server file
-        progress.update(len(bytesRead)) # update progress bar 
-
-convSock.close()
-serverSock.close()
-    
-
-
-    
-
+if __name__ == "__main__":
+    Main()
